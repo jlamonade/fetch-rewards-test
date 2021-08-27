5abdd84e04f0a5c550c771abb2843bb02db86db8
Jason Lam <nyjasonlam@gmail.com>
2021-08-26 20:19:41 -0400

    implemented transaction sorting

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 
 class Transaction {
-  constructor(payer, points) {
+  constructor(payer, points, timestamp) {
     this.payer = payer
     this.points = points
-    this.timestamp = new Date(Date.now())
+    this.timestamp = timestamp
   }
 }
 
@@ -12,14 +12,28 @@ const payerBalances = {}
 
 const transactions = []
 
+
+
 router.post('/', async (req, res) => {
-  await transactions.push(new Transaction(req.body.payer, req.body.points))
+  await transactions.push(new Transaction(req.body.payer, req.body.points, req.body.timestamp))
   if (!(req.body.payer in payerBalances)) {
     payerBalances[req.body.payer] = req.body.points
   } else {
     payerBalances[req.body.payer] = payerBalances[req.body.payer] + req.body.points
   }
   res.status(200).json(payerBalances)
+})
+
+router.post('/spend', async (req, res) => {
+  // sort transactions when points are being spent, by date
+  transactions.sort((a, b) => a.timestamp.localeCompare(b.timestamp))
+  // spend points
+    // go through transactions starting at earliest date
+    // check payer balance
+    // if 0 then go to next payer
+    // else spend all available points, do not let balance be negative
+    // then go to next payer
+  res.status(200).json(transactions)
 })
 
 router.get('/', (req, res) => {
