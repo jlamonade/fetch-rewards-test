19dbfaa15fb02a0fcb34d68abbb34cf8c5f94548
Jason Lam <nyjasonlam@gmail.com>
2021-08-27 13:26:19 -0400

    fixed data flow in spend POST route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const Transaction = require('../models')
 
-const payerBalances = {}
+const payerBalances = { totalAvailablePoints: 0 }
 
 const transactions = []
 
@@ -9,10 +9,13 @@ router.post('/', async (req, res) => {
   await transactions.push(new Transaction(req.body.payer, req.body.points, req.body.timestamp))
   if (req.body.payer in payerBalances) {
     payerBalances[req.body.payer] = payerBalances[req.body.payer] + req.body.points
+    payerBalances.totalAvailablePoints += req.body.points
   } else {
     payerBalances[req.body.payer] = req.body.points
+    payerBalances.totalAvailablePoints += req.body.points
   }
-  res.status(200).json(transactions)
+  console.log(payerBalances.totalAvailablePoints)
+  res.status(200).json(payerBalances.totalAvailablePoints)
 })
 
 router.post('/spend', async (req, res) => {
@@ -22,20 +25,28 @@ router.post('/spend', async (req, res) => {
   transactions.sort((a, b) => a.timestamp.localeCompare(b.timestamp))
   // spend points
   // go through transactions starting at earliest date
-  for (const transaction of transactions) {
-    // check payer balance
-    const availablePoints = Math.abs(transaction.points - transaction.pointsSpent) // 500
-    // console.log(availablePoints, transaction.points)
-    if (availablePoints > 0) {
-      if (pointsToSpend >= availablePoints || transaction.points < 0) {
-        transaction.pointsSpent += transaction.points
-        pointsToSpend -= transaction.pointsSpent
-      } else if (pointsToSpend < availablePoints) {
-        transaction.pointsSpent += pointsToSpend
-        pointsToSpend -= transaction.pointsSpent
+  if (payerBalances.totalAvailablePoints > pointsToSpend) {
+    payerBalances.totalAvailablePoints -= pointsToSpend
+    for (const transaction of transactions) {
+      // check payer balance
+      const availablePoints = transaction.points - transaction.pointsSpent // points available on specific transaction
+      console.log(transaction.payer, availablePoints)
+      if (availablePoints > 0 && pointsToSpend > 0) { // if not 0
+        if (pointsToSpend >= availablePoints) {
+          transaction.pointsSpent += availablePoints
+          pointsToSpend -= availablePoints
+          payerBalances[transaction.payer] -= availablePoints
+        } else if (pointsToSpend < availablePoints) {
+          payerBalances[transaction.payer] -= pointsToSpend
+          transaction.pointsSpent += pointsToSpend
+          pointsToSpend -= transaction.pointsSpent
+        } else if (transaction.points < 0 && availablePoints > 0) {
+          transaction.pointsSpent += transaction.points
+          pointsToSpend -= transaction.points
+          payerBalances[transaction.payer] -= transaction.pointsSpent
+        }
       }
     }
-    payerBalances[transaction.payer] -= transaction.pointsSpent
   }
   console.log(transactions)
   // if 0 then go to next payer
@@ -45,7 +56,7 @@ router.post('/spend', async (req, res) => {
 })
 
 router.get('/', (req, res) => {
-  res.status(200).json(transactions)
+  res.status(200).json(payerBalances)
 })
 
 module.exports = router
