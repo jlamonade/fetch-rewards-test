1a0a7fa2e3c576f28e8a8a602b384b3bf78c68d6
Jason Lam <nyjasonlam@gmail.com>
2021-09-03 12:26:04 -0400

    commented model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 class Transaction {
   // transaction 'model' for storing transactions to memory
+  // if using a database, also have an owner field
+  // consider using a boolean field so that the transaction can be filtered out in the query
+  // the db routes will create queries that filter by user index
   constructor (payer, points, timestamp) {
     this.payer = payer
     this.points = points
